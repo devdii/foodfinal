c1d6673d2219ea8f15e1b188239180cc66ce406e
Dev Chauhan <161327377+devdii@users.noreply.github.com>
2025-03-10 10:47:15 +0530

    Update email_config.php

--- food order system/email_config.php
+++ food order system/email_config.php
@@ -2,8 +2,8 @@
 // Email configuration settings
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_PORT', 465); // Updated to 465 for SMTPS
-define('SMTP_USERNAME', 'welcometoomyportfolio@gmail.com');
-define('SMTP_PASSWORD', 'nvdz zfui saxw ylak');
-define('SMTP_FROM_EMAIL', 'welcometoomyportfolio@gmail.com');
+define('SMTP_USERNAME', 'your email');
+define('SMTP_PASSWORD', 'password');
+define('SMTP_FROM_EMAIL', 'your email');
 define('SMTP_FROM_NAME', 'Food Order System');
 ?>
